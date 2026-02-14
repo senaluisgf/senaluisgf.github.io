ba66bf63bfd8c2ef1719e8dd50cda4314b747d0a
Luis "Shadow" Sena <sena.luisgf@gmail.com>
2026-02-14 16:52:52 -0400

    refactor: change main picture and add informations about me and my professional experience

--- js/script.js
+++ js/script.js
@@ -48,3 +48,21 @@ function navSectionToggleBtn() {
     allSections[i].classList.toggle("open");
   }
 }
+
+const ageEl = document.getElementById("age");
+const birth = new Date(ageEl.dataset.birth);
+const today = new Date();
+let age = today.getFullYear() - birth.getFullYear();
+const m = today.getMonth() - birth.getMonth();
+if (m < 0 || (m === 0 && today.getDate() < birth.getDate())) {
+  age--;
+}
+ageEl.textContent = age;
+const ageWorkEl = document.getElementById("age-work");
+const first = new Date(ageWorkEl.dataset.first);
+let ageWork = today.getFullYear() - first.getFullYear();
+const mWork = today.getMonth() - first.getMonth();
+if (mWork < 0 || (mWork === 0 && today.getDate() < first.getDate())) {
+  ageWork--;
+}
+ageWorkEl.textContent = ageWork;
